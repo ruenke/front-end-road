6bec6b417f6c0bc6d9cfedc83e22a366c2fc6102
ruenke <wuhouchu@163.com>
2019-05-11 22:04:28 +0800

    delate vue

--- vue/config/prod.env.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-let HOST = process.argv.splice(2)[0] || 'prod';
-module.exports = {
-  NODE_ENV: '"production"',
-  LARK_APPID: JSON.stringify("cli_9c04fe6ac6715101"),   //生产环境
-  HOST: '"'+HOST+'"'
-}
--- vue/config/test.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',    
-  LARK_APPID: JSON.stringify("cli_9cc2d7f060fc5108")   //测试环境
-})
